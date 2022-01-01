eb2b7692a2ed238e9117aac100e3c664272a8a34
Fatemeh Brizendine <ssaberi324@yahoo.com>
2022-01-01 13:06:18 -0800

    add initialize function

--- app.js
+++ app.js
@@ -7,7 +7,34 @@ const db = require('./db/connection');
 // })
 
 // initialize inquirer prompt
+function init() {
+        return inquirer.prompt(questions).then((answers) => {
+                console.log(answers)
+        })
+        .catch((err) => {
+                console.log(err);
+        })
+}
 
 // ask series of questions
+const questions = [
+        {
+                type: 'checkbox',
+                name: 'start',
+                message: 'What would you like to do?',
+                choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+
+        },
+        {
+
+        },
+        {
+
+        },
+
+]
 
 // switch case, depending on user selection
+
+// function call to initialize app
+init();
